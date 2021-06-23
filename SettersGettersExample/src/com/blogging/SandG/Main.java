b4f7c86334414bb1e28ab6525407322a03559248
Dikshitha Rao <dikshitharao48798@gmail.com>
2021-06-23 18:28:55 +0530

    Add files via upload

---
+++ SettersGettersExample/src/com/blogging/SandG/Main.java
@@ -0,0 +1,16 @@
+package com.blogging.SandG;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new Person();
+		person.setName("Java");
+		person.setId(1);
+		person.setAdress("Bangalore");
+		
+		System.out.println("Name: "+person.getName()+" ID: "+person.getId()+" Address: "+person.getAdress());
+
+	}
+
+}
---
+++ SettersGettersExample/src/com/blogging/SandG/Person.java
@@ -0,0 +1,33 @@
+package com.blogging.SandG;
+
+public class Person {
+	private String name;
+	private int id;
+	private String Adress;
+	
+	public String getName() {//getter for name
+		return name;	
+	}
+	public void setName(String name) {//setter for name
+		this.name = name;
+	}
+	public int getId() {//getter for id
+		return id;
+	}
+	public void setId(int id) {//setter for id
+		this.id = id;
+	}
+	public String getAdress() {//getter for address
+		return Adress;
+	}
+	public void setAdress(String adress) {//setter for address
+		Adress = adress;
+	}
+	
+	
+	
+	
+	
+	
+
+}
